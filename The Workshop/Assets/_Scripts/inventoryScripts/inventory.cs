8f730016f6eefe8d5653d8977e9c93b51c3d7463
vgmadlads4 <vgmadlads@gmail.com>
2024-02-21 20:35:29 -0500

    Inventory Script, InventoryObject Class script, and objectProperties script
    I created an Inventory script with code that would also allow for the inserting of game objects into the inventory of the player. The game object properties are represented with the objectProjerties class.

---
+++ The Workshop/Assets/_Scripts/inventoryScripts/inventory.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    public inventoryItems[,] thePlayerInventory = new inventoryItems[3, 10];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void storingObjects(objectProperties theObjectToBeStored)
+    {
+        for (int i = 0; i < thePlayerInventory.Length; i++)
+        {
+            for (int j = 0; j < thePlayerInventory.GetLength(i); j++)       
+            {
+                if(thePlayerInventory[i,j] == null)
+                {
+                    inventoryItems inventorySpot = new inventoryItems();
+                    inventorySpot.tag = theObjectToBeStored.tag;
+                    inventorySpot.name = theObjectToBeStored.name;
+                    inventorySpot.description = theObjectToBeStored.description;
+                    inventorySpot.theObjectImage = theObjectToBeStored.theObjectImage;
+                    inventorySpot.row = j;
+                    inventorySpot.column = i;
+                    thePlayerInventory[i, j] = inventorySpot;
+                }
+            }
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKeyDown(KeyCode.E) && other.tag == "interactableObject")
+        {
+            storingObjects(other.GetComponent<objectProperties>());
+        }
+    }
+
+}
